ac6eab3f56be7785522825ca4308919415225b0f
RenukaMarada <renuka.marada09@gmail.com>
2024-09-11 22:29:33 +0530

    provider, security group and EC2 instance

---
+++ sg.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow_ssh_terraform" {
+    name = "allow_ssh1"
+    description ="allow port 22 to ssh"
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"] 
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "allow_sshh"
+    }
+
+}
