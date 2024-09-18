64bbaf80584e3b433ab62b0b5e09b4f1f9f63a28
RenukaMarada <renuka.marada09@gmail.com>
2024-09-18 11:50:42 +0530

    ec2, count and conditions

---
+++ conditions/ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "terraform"{
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = var.environment == "dev" ? "t3.micro" : "t2.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = {
+        Name = "terraform"
+    } 
+}
--- count/ec2.tf
+++ count/ec2.tf
@@ -1,3 +1,13 @@
+resource "aws_instance" "terraform"{
+    count = length(var.instance_names)
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = {
+        Name = var.instance_names[count.index]
+    } 
+}
+
 resource "aws_security_group" "allow_ssh_terraform" {
     name = "allow_ssh1"
     description ="allow port 22 to ssh"
@@ -18,7 +28,6 @@ resource "aws_security_group" "allow_ssh_terraform" {
     }
 
     tags = {
-        Name = "allow_sshh"
+        Name = "allow_ssh1"
     }
-
 }
